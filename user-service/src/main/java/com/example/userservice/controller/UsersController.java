867abed757e4eb80a55837037bfaaa9e903a70bc
Minsuk kim <alstjrdl951@gmail.com>
2023-06-28 10:27:12 +0900

    feat: user controller 구현

--- user-service/src/main/java/com/example/userservice/controller/UsersController.java
+++ user-service/src/main/java/com/example/userservice/controller/UsersController.java
@@ -1,12 +1,18 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class UsersController {
+
+    private final Greeting greeting;
 
     @GetMapping("/health_check")
     public String status() {
@@ -15,6 +21,6 @@ public class UsersController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "";
+        return greeting.getMessage();
     }
 }
