c599a5f3d121f3ddc73aaf63e604dc3c476cdb37
Minsuk kim <alstjrdl951@gmail.com>
2023-07-02 16:17:47 +0900

    feat: spring cloud bus 추가

--- user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -20,7 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Date;
 
 @RequiredArgsConstructor
